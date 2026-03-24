5dcb13ad2c18e1f11852ef4c6e2aabe66c9af24a
dmohan1111 <52620342+dmohan1111@users.noreply.github.com>
2026-03-24 08:23:47 +0300

    updated the url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/esgtraining',
+  base: '/esg-training',
   server: {
     port: 5173,
     proxy: {
